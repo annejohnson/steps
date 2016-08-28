69ca7058b0397cf8edbcc9c7d143a5bc4a4a59a6
Anne Johnson <annekjohnson23@yahoo.com>
2016-08-28 17:22:24 -0400

    Require user to be logged in on users#index and users#show, use authentication plugs

--- web/router.ex
+++ web/router.ex
@@ -7,6 +7,7 @@ defmodule Steps.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Steps.Auth, repo: Steps.Repo
   end
 
   pipeline :api do
