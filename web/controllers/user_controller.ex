44dd2b5d4cfb231e487524cbb19618c1111b38a8
Anne Johnson <annekjohnson23@yahoo.com>
2016-08-28 16:28:37 -0400

    Add user#show action and template, and use a partial

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -5,4 +5,9 @@ defmodule Steps.UserController do
     users = Repo.all(Steps.User)
     render conn, "index.html", users: users
   end
+
+  def show(conn, %{"id" => id}) do
+    user = Repo.get(Steps.User, id)
+    render conn, "show.html", user: user
+  end
 end
