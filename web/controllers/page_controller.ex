0fa530b1165cb3131ed3b4ccc8d0cf4c27c4ef1d
Anne Johnson <annekjohnson23@yahoo.com>
2016-08-28 20:46:18 -0400

    Redirect user to their goal index when logged in

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,12 @@ defmodule Steps.PageController do
   use Steps.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    case conn.assigns.current_user do
+      nil ->
+        render conn, "index.html"
+      _ ->
+        conn
+        |> redirect(to: goal_path(conn, :index))
+    end
   end
 end
